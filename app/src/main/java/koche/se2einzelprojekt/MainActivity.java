c136300fddc3ec691341430eb758d0f4a05cbb26
Koche <thomaskoch96@outlook.com>
2018-03-11 13:54:38 +0100

    - Added layout - Added check for palindrom

--- app/src/main/java/koche/se2einzelprojekt/MainActivity.java
+++ app/src/main/java/koche/se2einzelprojekt/MainActivity.java
@@ -1,7 +1,11 @@
 package koche.se2einzelprojekt;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**Called when the user taps the Check button */
+    public void checkPladindrom(View view){
+
+        EditText editPalindorm = (EditText) findViewById(R.id.editPalindrom);
+        String palindrom = editPalindorm.getText().toString();
+
+        TextView resultView = findViewById(R.id.resultView);
+
+        if(palindrom.length() < 5 || palindrom == null){
+            resultView.setTextColor(Color.RED);
+            resultView.setText("Unzulässige Länge!");
+        }else{
+
+            if(isPladindrome(palindrom) == true){
+                resultView.setTextColor(Color.GREEN);
+                resultView.setText("Eingabe ist ein Palindrom!");
+            }else{
+                resultView.setTextColor(Color.RED);
+                resultView.setText("Eingabe ist kein Palindrom!");
+            }
+        }
+    }
+
+    /**Check if string is a palindrom*/
+    public static boolean isPladindrome(String str){
+        return str.equals(new StringBuilder(str).reverse().toString());
     }
 }
